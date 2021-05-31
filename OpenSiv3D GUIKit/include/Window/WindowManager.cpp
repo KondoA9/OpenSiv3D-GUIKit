a5fc175ae4314a5c4870198318286d3167c9fe47
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2021-05-31 11:55:59 +0900

    update

--- OpenSiv3D GUIKit/include/Window/WindowManager.cpp
+++ OpenSiv3D GUIKit/include/Window/WindowManager.cpp
@@ -1,6 +1,8 @@
 #include "WindowManager.h"
 
+#if SIV3D_PLATFORM(WINDOWS)
 #include <ShellScalingApi.h>
+#endif
 
 namespace s3d::gui::WindowManager {
 	const Size baseWindowSize = Size(1280, 720);
@@ -8,7 +10,10 @@ namespace s3d::gui::WindowManager {
 	bool resized = false;
 
 	void Initialize() {
+#if SIV3D_PLATFORM(WINDOWS)
 		SetProcessDPIAware();
+#endif
+
 		Window::Resize(baseWindowSize, WindowResizeOption::ResizeSceneSize);
 		Window::SetStyle(WindowStyle::Sizable);
 	}
