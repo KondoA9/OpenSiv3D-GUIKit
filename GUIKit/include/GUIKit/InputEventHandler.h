a9017887e1e9172f662e102573538666f2dbc674
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2021-07-08 16:36:06 +0900

    fix

--- GUIKit/include/GUIKit/InputEventHandler.h
+++ GUIKit/include/GUIKit/InputEventHandler.h
@@ -5,17 +5,12 @@
 namespace s3d::gui {
 	class InputEventHandler {
 	public:
-		const size_t eventTypeId;
-		const std::function<void(const InputEvent&)> handler;
+		size_t eventTypeId;
+		std::function<void(const InputEvent&)> handler;
 
 		template<class T>
 		static InputEventHandler Create(const std::function<void(InputEvent)>& _handler) noexcept {
 			return InputEventHandler(_handler, typeid(T).hash_code());
-		}
-
-		const InputEventHandler& operator =(const InputEventHandler& e) {
-			assert(eventTypeId == e.eventTypeId);
-			return *this;
 		}
 
 	private:
