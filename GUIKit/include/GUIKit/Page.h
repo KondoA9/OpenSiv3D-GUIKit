0360da9c72725d38a30c0dae857ca4f355361a1f
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2021-07-14 17:44:34 +0900

    Update Page Implement identifier() and didLoaded() Create Page.cpp

--- GUIKit/include/GUIKit/Page.h
+++ GUIKit/include/GUIKit/Page.h
@@ -43,17 +43,19 @@ namespace s3d::gui {
 		// Called when application terminated
 		virtual void onAppTerminated() {}
 
+		String identifier() const {
+			return m_identifier;
+		}
+
+		bool didLoaded() const {
+			return m_loaded;
+		}
+
 		UIView& view() {
 			return m_view;
 		}
 
 	private:
-		Page(const String& identifier) :
-			m_identifier(identifier) {
-			m_view.setConstraint(LayerDirection::Top);
-			m_view.setConstraint(LayerDirection::Bottom, [] {return Window::ClientHeight(); });
-			m_view.setConstraint(LayerDirection::Left);
-			m_view.setConstraint(LayerDirection::Right, [] {return Window::ClientWidth(); });
-		}
+		Page(const String& identifier);
 	};
 }
