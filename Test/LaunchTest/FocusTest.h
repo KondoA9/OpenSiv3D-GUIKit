15980e3ba1dcb08df26100bdfaf14ba5504efe41
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2021-07-26 20:41:43 +0900

    Fixed FocusTest

--- Test/LaunchTest/FocusTest.h
+++ Test/LaunchTest/FocusTest.h
@@ -10,32 +10,32 @@ void FocusTest::onLoaded() {
 
 	const size_t focusTimeout = gui::GUIKit::SetTimeout([] {
 		Test::Assert(false, "Focused the component, but event was not triggered");
-		}, 5000, false);
+		}, 3000, false);
 
 	gui::GUIKit::SetTimeout([&rect] {
 		rect.focus();
 		Test::Assert(rect.isFocused(), "Focused a component, but it is not focused");
-		}, 2500, false);
+		}, 1500, false);
 
 	rect.addEventListener<gui::Focused>([&rect, focusTimeout] {
 		gui::GUIKit::StopTimeout(focusTimeout);
 
 		const size_t unfocusTimeout = gui::GUIKit::SetTimeout([] {
 			Test::Assert(false, "UnFocused the component, but event was not triggered");
-			}, 5000, false);
+			}, 3000, false);
 
 		gui::GUIKit::SetTimeout([&rect] {
 			rect.unFocus();
 			Test::Assert(!rect.isFocused(), "UnFocused a component, but it is focused");
-			}, 2500, false);
+			}, 1500, false);
 
 		rect.addEventListener<gui::UnFocused>([unfocusTimeout] {
 			gui::GUIKit::StopTimeout(unfocusTimeout);
 			});
 		});
 
-	// 10sec later, terminate app
+	// 5sec later, terminate app
 	gui::GUIKit::SetTimeout([] {
 		gui::GUIKit::Terminate();
-		}, 10000, true);
+		}, 5000, true);
 }
