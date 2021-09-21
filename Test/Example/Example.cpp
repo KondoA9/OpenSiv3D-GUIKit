ed85539e8912e0e4a5021dd43967834140fb4ac9
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2021-09-21 21:07:26 +0900

    Fixed Example

--- Test/Example/Example.cpp
+++ Test/Example/Example.cpp
@@ -1,13 +1,14 @@
 #include <GUIKit/GUIKit.hpp>
 
 class ToolBar final : public gui::UIView {
+private:
 	std::function<void()> m_toggleColorModeHandler;
 	std::function<void(const FilePath&)> m_folderOpenedHandler;
 	std::function<void()> m_openParentDirHandler;
 
-	gui::UIToggleButton& ui_toggleColorModeButton = gui::GUIFactory::Create<gui::UIToggleButton>(this);
-	gui::UIButton& ui_parentDirButton = gui::GUIFactory::Create<gui::UIButton>(this);
-	gui::UIButton& ui_openDirectoryButton = gui::GUIFactory::Create<gui::UIButton>(this);
+	gui::UIToggleButton& m_uiToggleColorModeButton = gui::GUIFactory::Create<gui::UIToggleButton>(this);
+	gui::UIButton& m_uiParentDirButton = gui::GUIFactory::Create<gui::UIButton>(this);
+	gui::UIButton& m_uiOpenDirectoryButton = gui::GUIFactory::Create<gui::UIButton>(this);
 
 public:
 	using UIView::UIView;
@@ -28,31 +29,31 @@ protected:
 	void initialize() override {
 		UIView::initialize();
 
-		ui_openDirectoryButton.setText(U"Open");
-		ui_parentDirButton.setIcon(Texture(Icon(0xf062), 20_px));
-		ui_toggleColorModeButton.setIcon(Texture(Icon(0xf186), 20_px));
+		m_uiOpenDirectoryButton.setText(U"Open");
+		m_uiParentDirButton.setIcon(Texture(Icon(0xf062), 20_px));
+		m_uiToggleColorModeButton.setIcon(Texture(Icon(0xf186), 20_px));
 
-		ui_toggleColorModeButton.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
-		ui_toggleColorModeButton.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
-		ui_toggleColorModeButton.setConstraint(gui::LayerDirection::Left);
-		ui_toggleColorModeButton.setConstraint(gui::LayerDirection::Width, 40_px);
-		ui_toggleColorModeButton.addEventListener<gui::MouseEvent::LeftDown>([this] {
+		m_uiToggleColorModeButton.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
+		m_uiToggleColorModeButton.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
+		m_uiToggleColorModeButton.setConstraint(gui::LayerDirection::Left);
+		m_uiToggleColorModeButton.setConstraint(gui::LayerDirection::Width, 40_px);
+		m_uiToggleColorModeButton.addEventListener<gui::MouseEvent::LeftDown>([this] {
 			m_toggleColorModeHandler();
 			});
 
-		ui_parentDirButton.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
-		ui_parentDirButton.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
-		ui_parentDirButton.setConstraint(gui::LayerDirection::Left, ui_toggleColorModeButton, gui::LayerDirection::Right);
-		ui_parentDirButton.setConstraint(gui::LayerDirection::Width, 40_px);
-		ui_parentDirButton.addEventListener<gui::MouseEvent::LeftDown>([this] {
+		m_uiParentDirButton.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
+		m_uiParentDirButton.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
+		m_uiParentDirButton.setConstraint(gui::LayerDirection::Left, m_uiToggleColorModeButton, gui::LayerDirection::Right);
+		m_uiParentDirButton.setConstraint(gui::LayerDirection::Width, 40_px);
+		m_uiParentDirButton.addEventListener<gui::MouseEvent::LeftDown>([this] {
 			m_openParentDirHandler();
 			});
 
-		ui_openDirectoryButton.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
-		ui_openDirectoryButton.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
-		ui_openDirectoryButton.setConstraint(gui::LayerDirection::Left, ui_parentDirButton, gui::LayerDirection::Right);
-		ui_openDirectoryButton.setConstraint(gui::LayerDirection::Width, 120_px);
-		ui_openDirectoryButton.addEventListener<gui::MouseEvent::LeftDown>([this] {
+		m_uiOpenDirectoryButton.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
+		m_uiOpenDirectoryButton.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
+		m_uiOpenDirectoryButton.setConstraint(gui::LayerDirection::Left, m_uiParentDirButton, gui::LayerDirection::Right);
+		m_uiOpenDirectoryButton.setConstraint(gui::LayerDirection::Width, 120_px);
+		m_uiOpenDirectoryButton.addEventListener<gui::MouseEvent::LeftDown>([this] {
 			const auto result = Dialog::SelectFolder();
 			if (result.has_value()) {
 				m_folderOpenedHandler(result.value());
@@ -62,35 +63,36 @@ protected:
 };
 
 class FileView final : public gui::UIView {
+private:
 	FilePath m_path;
 	std::function<void(const FilePath& path)> m_folderSelectedHandler;
 
-	gui::UIIcon& ui_icon = gui::GUIFactory::Create<gui::UIIcon>(this);
-	gui::UIText& ui_fileName = gui::GUIFactory::Create<gui::UIText>(this);
-	gui::UIText& ui_updatedDate = gui::GUIFactory::Create<gui::UIText>(this);
-	gui::UIText& ui_kind = gui::GUIFactory::Create<gui::UIText>(this);
+	gui::UIIcon& m_uiIcon = gui::GUIFactory::Create<gui::UIIcon>(this);
+	gui::UIText& m_uiFileName = gui::GUIFactory::Create<gui::UIText>(this);
+	gui::UIText& m_uiUpdatedDate = gui::GUIFactory::Create<gui::UIText>(this);
+	gui::UIText& m_uiKind = gui::GUIFactory::Create<gui::UIText>(this);
 
 public:
 	FileView()
 		:UIView()
 	{
-		ui_updatedDate.backgroundColor = gui::DynamicColor::Background;
-		ui_kind.backgroundColor = gui::DynamicColor::Background;
+		m_uiUpdatedDate.backgroundColor = gui::DynamicColor::Background;
+		m_uiKind.backgroundColor = gui::DynamicColor::Background;
 	}
 
 	void setPath(const FilePath& path) {
-		ui_icon.setIcon(FileSystem::IsDirectory(path) ? Icon(0xf07b) : Icon(0xf15b), 20_px);
-		ui_icon.iconColor = FileSystem::IsDirectory(path) ? gui::ColorTheme(Color(246, 218, 75)) : gui::DynamicColor::Text;
+		m_uiIcon.setIcon(FileSystem::IsDirectory(path) ? Icon(0xf07b) : Icon(0xf15b), 20_px);
+		m_uiIcon.iconColor = FileSystem::IsDirectory(path) ? gui::ColorTheme(Color(246, 218, 75)) : gui::DynamicColor::Text;
 
-		ui_fileName.setText(FileSystem::FileName(path));
+		m_uiFileName.setText(FileSystem::FileName(path));
 
-		ui_kind.setText(FileSystem::IsDirectory(path) ? U"Folder" : U"File");
+		m_uiKind.setText(FileSystem::IsDirectory(path) ? U"Folder" : U"File");
 
 		m_path = path;
 
 		const auto time = FileSystem::WriteTime(path);
 		if (time.has_value()) {
-			ui_updatedDate.setText(time.value().format());
+			m_uiUpdatedDate.setText(time.value().format());
 		}
 	}
 
@@ -114,87 +116,90 @@ protected:
 			}
 			});
 
-		ui_icon.penetrateMouseEvent = true;
-		ui_icon.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
-		ui_icon.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
-		ui_icon.setConstraint(gui::LayerDirection::Left, *this, gui::LayerDirection::Left);
-		ui_icon.setConstraint(gui::LayerDirection::Width, *this, gui::LayerDirection::Height);
+		m_uiIcon.penetrateMouseEvent = true;
+		m_uiIcon.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
+		m_uiIcon.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
+		m_uiIcon.setConstraint(gui::LayerDirection::Left, *this, gui::LayerDirection::Left);
+		m_uiIcon.setConstraint(gui::LayerDirection::Width, *this, gui::LayerDirection::Height);
 
-		ui_fileName.penetrateMouseEvent = true;
-		ui_fileName.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
-		ui_fileName.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
-		ui_fileName.setConstraint(gui::LayerDirection::Left, ui_icon, gui::LayerDirection::Right);
-		ui_fileName.setConstraint(gui::LayerDirection::Right, ui_updatedDate, gui::LayerDirection::Left);
+		m_uiFileName.penetrateMouseEvent = true;
+		m_uiFileName.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
+		m_uiFileName.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
+		m_uiFileName.setConstraint(gui::LayerDirection::Left, m_uiIcon, gui::LayerDirection::Right);
+		m_uiFileName.setConstraint(gui::LayerDirection::Right, m_uiUpdatedDate, gui::LayerDirection::Left);
 
-		ui_updatedDate.penetrateMouseEvent = true;
-		ui_updatedDate.setPadding(0, 0, 5_px, 5_px);
-		ui_updatedDate.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
-		ui_updatedDate.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
-		ui_updatedDate.setConstraint(gui::LayerDirection::Width, 220_px);
-		ui_updatedDate.setConstraint(gui::LayerDirection::Right, ui_kind, gui::LayerDirection::Left);
+		m_uiUpdatedDate.penetrateMouseEvent = true;
+		m_uiUpdatedDate.setPadding(0, 0, 5_px, 5_px);
+		m_uiUpdatedDate.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
+		m_uiUpdatedDate.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
+		m_uiUpdatedDate.setConstraint(gui::LayerDirection::Width, 220_px);
+		m_uiUpdatedDate.setConstraint(gui::LayerDirection::Right, m_uiKind, gui::LayerDirection::Left);
 
-		ui_kind.penetrateMouseEvent = true;
-		ui_kind.setPadding(0, 0, 5_px, 5_px);
-		ui_kind.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
-		ui_kind.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
-		ui_kind.setConstraint(gui::LayerDirection::Width, 80_px);
-		ui_kind.setConstraint(gui::LayerDirection::Right, *this, gui::LayerDirection::Right);
+		m_uiKind.penetrateMouseEvent = true;
+		m_uiKind.setPadding(0, 0, 5_px, 5_px);
+		m_uiKind.setConstraint(gui::LayerDirection::Top, *this, gui::LayerDirection::Top);
+		m_uiKind.setConstraint(gui::LayerDirection::Bottom, *this, gui::LayerDirection::Bottom);
+		m_uiKind.setConstraint(gui::LayerDirection::Width, 80_px);
+		m_uiKind.setConstraint(gui::LayerDirection::Right, *this, gui::LayerDirection::Right);
 	}
 };
 
 class ExplorerPage : public gui::Page {
+private:
 	using Page::Page;
 
 	FilePath m_path;
 
-	ToolBar& ui_toolbar = gui::GUIFactory::Create<ToolBar>(view);
-	gui::UIVStackView& ui_filesView = gui::GUIFactory::Create<gui::UIVStackView>(view);
-	gui::UIButton& ui_movePage = gui::GUIFactory::Create<gui::UIButton>(view);
+	ToolBar& m_uiToolbar = gui::GUIFactory::Create<ToolBar>(view);
+	gui::UIVStackView& m_uiFilesView = gui::GUIFactory::Create<gui::UIVStackView>(view);
+	gui::UIButton& m_uiMovePage = gui::GUIFactory::Create<gui::UIButton>(view);
 
+protected:
 	void onLoaded() override {
-		ui_toolbar.backgroundColor = gui::DynamicColor::BackgroundSecondary;
-		ui_movePage.setText(U"Move page");
+		m_uiToolbar.backgroundColor = gui::DynamicColor::BackgroundSecondary;
+		m_uiMovePage.setText(U"Move page");
 
-		ui_toolbar.drawFrame = true;
-		ui_toolbar.setConstraint(gui::LayerDirection::Top, view, gui::LayerDirection::Top);
-		ui_toolbar.setConstraint(gui::LayerDirection::Height, 30_px);
-		ui_toolbar.setConstraint(gui::LayerDirection::Left);
-		ui_toolbar.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
-		ui_toolbar.setToggleColorModeHandler([this] {
+		m_uiToolbar.drawFrame = true;
+		m_uiToolbar.setConstraint(gui::LayerDirection::Top, view, gui::LayerDirection::Top);
+		m_uiToolbar.setConstraint(gui::LayerDirection::Height, 30_px);
+		m_uiToolbar.setConstraint(gui::LayerDirection::Left);
+		m_uiToolbar.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
+		m_uiToolbar.setToggleColorModeHandler([this] {
 			gui::GUIKit::ToggleColorMode();
 			});
-		ui_toolbar.setFolderOpendHandler([this](const FilePath& path) {
+		m_uiToolbar.setFolderOpendHandler([this](const FilePath& path) {
 			setup(path);
 			});
-		ui_toolbar.setOpenParentDirHandler([this] {
+		m_uiToolbar.setOpenParentDirHandler([this] {
 			const FilePath path = FileSystem::ParentPath(m_path);
 			setup(path);
 			});
 
-		ui_filesView.setLeadingDirection(gui::LeadingDirection::Bottom);
-		ui_filesView.setRowHeight(30_px);
-		ui_filesView.setConstraint(gui::LayerDirection::Top, ui_toolbar, gui::LayerDirection::Bottom);
-		ui_filesView.setConstraint(gui::LayerDirection::Bottom, ui_movePage, gui::LayerDirection::Top);
-		ui_filesView.setConstraint(gui::LayerDirection::Left, view, gui::LayerDirection::Left);
-		ui_filesView.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
+		m_uiFilesView.setLeadingDirection(gui::LeadingDirection::Bottom);
+		m_uiFilesView.setRowHeight(30_px);
+		m_uiFilesView.setConstraint(gui::LayerDirection::Top, m_uiToolbar, gui::LayerDirection::Bottom);
+		m_uiFilesView.setConstraint(gui::LayerDirection::Bottom, m_uiMovePage, gui::LayerDirection::Top);
+		m_uiFilesView.setConstraint(gui::LayerDirection::Left, view, gui::LayerDirection::Left);
+		m_uiFilesView.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
 
-		ui_movePage.setConstraint(gui::LayerDirection::Height, 40_px);
-		ui_movePage.setConstraint(gui::LayerDirection::Bottom, view, gui::LayerDirection::Bottom);
-		ui_movePage.setConstraint(gui::LayerDirection::Left, view, gui::LayerDirection::Left);
-		ui_movePage.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
-		ui_movePage.addEventListener<gui::MouseEvent::LeftDown>([this] {
+		m_uiMovePage.setConstraint(gui::LayerDirection::Height, 40_px);
+		m_uiMovePage.setConstraint(gui::LayerDirection::Bottom, view, gui::LayerDirection::Bottom);
+		m_uiMovePage.setConstraint(gui::LayerDirection::Left, view, gui::LayerDirection::Left);
+		m_uiMovePage.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
+		m_uiMovePage.addEventListener<gui::MouseEvent::LeftDown>([this] {
 			gui::GUIKit::SwitchPage(U"start");
 			});
 	}
 
+private:
 	void setup(const FilePath& dir) {
 		m_path = dir;
 
-		ui_filesView.release();
+		m_uiFilesView.release();
 
 		const auto contents = FileSystem::DirectoryContents(dir, Recursive::No);
 		for (const auto& path : contents) {
-			auto& row = gui::GUIFactory::Create<FileView>(ui_filesView);
+			auto& row = gui::GUIFactory::Create<FileView>(m_uiFilesView);
 			row.setPath(path);
 			row.penetrateMouseEvent = true;
 			row.setFolderSelectedHandler([this](FilePath path) {
@@ -205,34 +210,36 @@ class ExplorerPage : public gui::Page {
 };
 
 class StartPage : public gui::Page {
+private:
 	using Page::Page;
 
-	gui::UIText& ui_title = gui::GUIFactory::Create<gui::UIText>(view);
-	gui::UIButton& ui_button = gui::GUIFactory::Create<gui::UIButton>(view);
-	gui::UICheckBox& ui_checkBox = gui::GUIFactory::Create<gui::UICheckBox>(view);
+	gui::UIText& m_uiTitle = gui::GUIFactory::Create<gui::UIText>(view);
+	gui::UIButton& m_uiButton = gui::GUIFactory::Create<gui::UIButton>(view);
+	gui::UICheckBox& m_uiCheckBox = gui::GUIFactory::Create<gui::UICheckBox>(view);
 
+protected:
 	void onLoaded() override {
-		ui_title.setText(U"This is the example application of OpenSiv3D GUIKit.");
-		ui_title.setFont(gui::UnifiedFontStyle::Header);
-		ui_title.setDirection(gui::TextDirection::Center);
+		m_uiTitle.setText(U"This is the example application of OpenSiv3D GUIKit.");
+		m_uiTitle.setFont(gui::UnifiedFontStyle::Header);
+		m_uiTitle.setDirection(gui::TextDirection::Center);
 
-		ui_button.setText(U"Next");
+		m_uiButton.setText(U"Next");
 
-		ui_title.setConstraint(gui::LayerDirection::Top);
-		ui_title.setConstraint(gui::LayerDirection::Height, view, gui::LayerDirection::Height, 0.0, 0.5);
-		ui_title.setConstraint(gui::LayerDirection::Left);
-		ui_title.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
+		m_uiTitle.setConstraint(gui::LayerDirection::Top);
+		m_uiTitle.setConstraint(gui::LayerDirection::Height, view, gui::LayerDirection::Height, 0.0, 0.5);
+		m_uiTitle.setConstraint(gui::LayerDirection::Left);
+		m_uiTitle.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
 
-		ui_checkBox.setConstraint(gui::LayerDirection::Height, 100_px);
-		ui_checkBox.setConstraint(gui::LayerDirection::Bottom, ui_button, gui::LayerDirection::Top);
-		ui_checkBox.setConstraint(gui::LayerDirection::Left);
-		ui_checkBox.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
+		m_uiCheckBox.setConstraint(gui::LayerDirection::Height, 100_px);
+		m_uiCheckBox.setConstraint(gui::LayerDirection::Bottom, m_uiButton, gui::LayerDirection::Top);
+		m_uiCheckBox.setConstraint(gui::LayerDirection::Left);
+		m_uiCheckBox.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
 
-		ui_button.setConstraint(gui::LayerDirection::Height, 100_px);
-		ui_button.setConstraint(gui::LayerDirection::Bottom, view, gui::LayerDirection::Bottom);
-		ui_button.setConstraint(gui::LayerDirection::Left);
-		ui_button.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
-		ui_button.addEventListener<gui::MouseEvent::LeftDown>([this] {
+		m_uiButton.setConstraint(gui::LayerDirection::Height, 100_px);
+		m_uiButton.setConstraint(gui::LayerDirection::Bottom, view, gui::LayerDirection::Bottom);
+		m_uiButton.setConstraint(gui::LayerDirection::Left);
+		m_uiButton.setConstraint(gui::LayerDirection::Right, view, gui::LayerDirection::Right);
+		m_uiButton.addEventListener<gui::MouseEvent::LeftDown>([this] {
 			gui::GUIKit::SwitchPage(U"explorer");
 			});
 	}
