faa257f384b4c7c76649eb6ce0b9ac064f3d10cd
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-07-04 17:05:33 +0900

    refactor(KeyShortcut): use SFINAE and delete KeyShortcut.cpp

--- Aoba/src/KeyShortcut/KeyShortcut.hpp
+++ Aoba/src/KeyShortcut/KeyShortcut.hpp
@@ -20,9 +20,12 @@ namespace s3d::aoba {
         }
     };
 
-	// compile fails if class T is not Input, InputCombination or InputGroup 
-    template <class T>
-    class KeyShortcut : public KeyShortcutBase {
+    // T must be s3d::Input, s3d::InputCombination or s3d::InputGroup.
+    template <class T,
+              typename =
+                  typename std::enable_if_t<std::is_same<T, Input>::value || std::is_same<T, InputCombination>::value
+                                            || std::is_same<T, InputGroup>::value>>
+    class KeyShortcut final : public KeyShortcutBase {
     private:
         const T m_input;
 
@@ -30,6 +33,8 @@ namespace s3d::aoba {
         KeyShortcut(const T& input, const std::function<void()>& callback) :
             KeyShortcutBase(callback), m_input(input) {}
 
-        bool keyDown() const override;
+        bool keyDown() const override {
+            return m_input.down();
+        }
     };
 }
