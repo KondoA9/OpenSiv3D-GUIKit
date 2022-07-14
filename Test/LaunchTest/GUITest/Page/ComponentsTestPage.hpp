5b38e29fee9b56eda8d4dee01364beb8c5d6ca0e
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-07-14 16:59:05 +0900

    test: update to use UIView::layout()

--- Test/LaunchTest/GUITest/Page/ComponentsTestPage.hpp
+++ Test/LaunchTest/GUITest/Page/ComponentsTestPage.hpp
@@ -12,59 +12,56 @@ class ComponentsTestPage final : public aoba::Page {
 
 void ComponentsTestPage::onLoaded() {
     auto& stackView = aoba::Factory::Create<aoba::UIVStackView>(view);
-    stackView.setConstraint(aoba::LayerDirection::Top, view, aoba::LayerDirection::Top);
-    stackView.setConstraint(aoba::LayerDirection::Bottom, view, aoba::LayerDirection::Bottom);
-    stackView.setConstraint(aoba::LayerDirection::Left, view, aoba::LayerDirection::Left);
-    stackView.setConstraint(aoba::LayerDirection::Width, view, aoba::LayerDirection::Width, 0.0, 0.5);
+
+    auto& rect         = aoba::Factory::Create<aoba::UIRect>(stackView);
+    auto& circle       = aoba::Factory::Create<aoba::UICircle>(stackView);
+    auto& button       = aoba::Factory::Create<aoba::UIButton>(stackView);
+    auto& toggleButton = aoba::Factory::Create<aoba::UIToggleButton>(stackView);
+    auto& text         = aoba::Factory::Create<aoba::UIText>(stackView);
+    auto& inputField   = aoba::Factory::Create<aoba::UIInputField>(stackView);
+    auto& checkBox     = aoba::Factory::Create<aoba::UICheckBox>(stackView);
+    auto& slider       = aoba::Factory::Create<aoba::UISlider>(stackView);
+    auto& icon         = aoba::Factory::Create<aoba::UIIcon>(stackView);
+    auto& imageView    = aoba::Factory::Create<aoba::UIImageView>(stackView);
 
     auto& tabView = aoba::Factory::Create<aoba::UITabView>(view);
-    tabView.setConstraint(aoba::LayerDirection::Top, view, aoba::LayerDirection::Top);
-    tabView.setConstraint(aoba::LayerDirection::Bottom, view, aoba::LayerDirection::Bottom);
-    tabView.setConstraint(aoba::LayerDirection::Left, stackView, aoba::LayerDirection::Right);
-    tabView.setConstraint(aoba::LayerDirection::Right, view, aoba::LayerDirection::Right);
+    auto& tab1    = tabView.appendTab<aoba::UIView>(U"Tab1");
+    auto& tab2    = tabView.appendTab<aoba::UIView>(U"Tab2");
 
-    auto& rect           = aoba::Factory::Create<aoba::UIRect>(stackView);
-    rect.backgroundColor = aoba::MaterialColor::Amber5;
+    auto& verticalAlignView      = aoba::Factory::Create<aoba::UIView>(view);
+    auto& verticalAlignViewRect1 = aoba::Factory::Create<aoba::UIRect>(verticalAlignView);
+    auto& verticalAlignViewRect2 = aoba::Factory::Create<aoba::UIRect>(verticalAlignView);
 
-    auto& circle           = aoba::Factory::Create<aoba::UICircle>(stackView);
-    circle.backgroundColor = aoba::MaterialColor::Blue5;
+    view.layout<aoba::AlignH>(
+        {aoba::LORatio(stackView, 0.4),
+         aoba::LORatio(tabView, 0.2),
+         aoba::LOFill(verticalAlignView.layout<aoba::AlignV>(
+             {aoba::LOConstant(verticalAlignViewRect1, 200_px), aoba::LOFill(verticalAlignViewRect2)}))});
 
-    auto& button           = aoba::Factory::Create<aoba::UIButton>(stackView);
-    button.backgroundColor = aoba::MaterialColor::BlueGrey5;
-    button.setText(U"UIButton");
-
-    auto& toggleButton           = aoba::Factory::Create<aoba::UIToggleButton>(stackView);
+    rect.backgroundColor         = aoba::MaterialColor::Amber5;
+    circle.backgroundColor       = aoba::MaterialColor::Blue5;
+    button.backgroundColor       = aoba::MaterialColor::BlueGrey5;
     toggleButton.backgroundColor = aoba::MaterialColor::Brown5;
-    toggleButton.setText(U"UIToggleButton");
+    text.backgroundColor         = aoba::MaterialColor::Cyan5;
+    inputField.backgroundColor   = aoba::MaterialColor::DeepOrange5;
+    checkBox.backgroundColor     = aoba::MaterialColor::DeepPurple5;
+    slider.backgroundColor       = aoba::MaterialColor::Grey5;
+    icon.backgroundColor         = aoba::MaterialColor::Green5;
+    imageView.backgroundColor    = aoba::MaterialColor::Indigo5;
 
-    auto& text           = aoba::Factory::Create<aoba::UIText>(stackView);
-    text.backgroundColor = aoba::MaterialColor::Cyan5;
-    text.setText(U"UIText");
-
-    auto& inputField           = aoba::Factory::Create<aoba::UIInputField>(stackView);
-    inputField.backgroundColor = aoba::MaterialColor::DeepOrange5;
-    inputField.setText(U"UIInputField");
-
-    auto& checkBox           = aoba::Factory::Create<aoba::UICheckBox>(stackView);
-    checkBox.backgroundColor = aoba::MaterialColor::DeepPurple5;
-    checkBox.setChecked(true);
-
-    auto& slider           = aoba::Factory::Create<aoba::UISlider>(stackView);
-    slider.backgroundColor = aoba::MaterialColor::Grey5;
-
-    auto& icon           = aoba::Factory::Create<aoba::UIIcon>(stackView);
-    icon.backgroundColor = aoba::MaterialColor::Green5;
-    icon.setIcon(Icon(0xf109), 40_px);
-
-    auto& imageView           = aoba::Factory::Create<aoba::UIImageView>(stackView);
-    imageView.backgroundColor = aoba::MaterialColor::Indigo5;
-    imageView.appendImage(Image(U"asset/image1.png"));
-
-    auto& tab1           = tabView.appendTab<aoba::UIView>(U"Tab1");
     tab1.backgroundColor = aoba::MaterialColor::Blue5;
-
-    auto& tab2           = tabView.appendTab<aoba::UIView>(U"Tab2");
     tab2.backgroundColor = aoba::MaterialColor::Blue5;
+
+    verticalAlignViewRect1.backgroundColor = aoba::MaterialColor::Green3;
+    verticalAlignViewRect2.backgroundColor = aoba::MaterialColor::Green7;
+
+    button.setText(U"UIButton");
+    toggleButton.setText(U"UIToggleButton");
+    text.setText(U"UIText");
+    inputField.setText(U"UIInputField");
+    checkBox.setChecked(true);
+    icon.setIcon(Icon(0xf109), 40_px);
+    imageView.appendImage(Image(U"asset/image1.png"));
 
     // 5sec later, switch page
     aoba::Core::SetTimeout([] { aoba::Core::SwitchPage(U"FocusTest"); }, 5000, true);
