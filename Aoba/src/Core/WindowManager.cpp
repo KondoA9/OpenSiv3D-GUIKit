2f6856951fb42f7ab3ab6144229881bee9da7a73
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-08-02 16:28:38 +0900

    refactor(WindowManager): fix variable name

--- Aoba/src/Core/WindowManager.cpp
+++ Aoba/src/Core/WindowManager.cpp
@@ -3,20 +3,20 @@
 #include <Siv3D.hpp>
 
 namespace s3d::aoba::WindowManager {
-    Size currentwindowSize = Size(0, 0);
-    bool resized           = false;
+    Size windowSize = Size(0, 0);
+    bool didResized = false;
 
     void Initialize() {
         Window::SetStyle(WindowStyle::Sizable);
     }
 
     void Update() {
-        const auto size   = Window::GetState().frameBufferSize;
-        resized           = currentwindowSize != size;
-        currentwindowSize = size;
+        const auto size = Window::GetState().frameBufferSize;
+        didResized      = windowSize != size;
+        windowSize      = size;
     }
 
     bool DidResized() {
-        return resized;
+        return didResized;
     }
 }
